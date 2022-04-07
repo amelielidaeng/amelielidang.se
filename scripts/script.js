e8f47906f3a693491f20c4b1492f31552beab06f
amelielidaeng <98023444+amelielidaeng@users.noreply.github.com>
2022-04-07 10:34:26 +0200

    work cards added and commented out

--- scripts/script.js
+++
@@ -1,8 +0,0 @@
-function burgerMenu() {
-    let burger = document.getElementById("nav-bar");
-    if (burger.className === "menu") {
-    burger.className += " responsive";
-    } else {
-    burger.className = "menu";
-    }
-}
